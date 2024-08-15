2e0c3219ef4591a817e90abbc04a004d8a55e90d
dale <dale@hmdtrucking.com>
2024-08-15 18:28:22 -0500

    rename

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dale\Efficiency\tablet-performance-monitor\venv
+set VIRTUAL_ENV=C:\Users\Dale\Efficiency\BoostMyTab\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
